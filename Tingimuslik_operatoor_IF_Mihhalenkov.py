dacd45b90f2a11c097fbd0894e651b76b3481096
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-01-23 09:51:45 +0200

    Update Tingimuslik_operatoor_IF_Mihhalenkov.py

--- Tingimuslik_operatoor_IF_Mihhalenkov.py
+++ Tingimuslik_operatoor_IF_Mihhalenkov.py
@@ -1,3 +1,24 @@
-﻿#3-ий Вариант.
+#3-ий Вариант.
 #Harjutus 1. 
 print("Harjutus 1.")
+n = int(input("Введи число: "))
+for i in range(n):
+    print("   ----- ", end="")
+    if i != n-1:
+        print("    ", end="")
+print("")
+for i in range(n):
+    print("  |  O  |", end="")
+    if i != n-1:
+        print("    ", end="")
+print("")
+for i in range(n):
+    print("  !  -  !", end="")
+    if i != n-1:
+        print("    ", end="")
+print("")
+for i in range(n):
+    print("   ----- ", end="")
+    if i != n-1:
+        print("    ", end="")
+print("")
